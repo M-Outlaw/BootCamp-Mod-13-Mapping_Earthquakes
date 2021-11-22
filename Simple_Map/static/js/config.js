6b75ce061d1c49d46ebb04a7ad3457a8ed1460f6
Michelle Outlaw <outlaw@Michelles-MBP.attlocal.net>
2021-11-22 17:04:39 -0500

    change

--- Simple_Map/static/js/config.js
+++ Simple_Map/static/js/config.js
@@ -1,2 +1,2 @@
 // API key
-const API_KEY = pk.eyJ1IjoibWhvdXRsYXciLCJhIjoiY2t3YjA1eWdsNDUxajJ2bXFiankybTh3eSJ9.V1Kh_7pL4M-9aumPP2UayA
+const API_KEY = "pk.eyJ1IjoibWhvdXRsYXciLCJhIjoiY2t3YjA1eWdsNDUxajJ2bXFiankybTh3eSJ9.V1Kh_7pL4M-9aumPP2UayA";
